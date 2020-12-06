9cebfb2c4e8f9b29081b15515d230951cadf6642
bluerawgamer <bluerawgamer@gmail.com>
2020-12-06 17:45:40 +0000

    Rule Evaluation and Adding to Count

--- src/main/java/Team_Troopers/ES_Project/PrimaryController.java
+++ src/main/java/Team_Troopers/ES_Project/PrimaryController.java
@@ -41,15 +41,17 @@ public class PrimaryController implements Initializable {
 	private Stage excelWindow;
 	private boolean set = false;
 	private Stage textualWindow;
+	private Stage userWindow;
 	private ArrayList<ExcelRecord> recordList;
 	private HashMap<EvalType, Integer> counting;
+	private ArrayList<String> userArray;
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		avaliarTools.getItems().addAll("Textual", "Tabular", "Gráfica");
 		recordList = new ArrayList<ExcelRecord>();
 		counting = new HashMap<EvalType, Integer>();
-		use_rules.selectedProperty().addListener(this::edit_count);;
+		use_rules.selectedProperty().addListener(this::edit_count);
 	}
 
 	public void closeMainWindow(WindowEvent e) {
@@ -64,21 +66,94 @@ public class PrimaryController implements Initializable {
 		else
 			e.consume();
 	}
-	
+
 	private void edit_count(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
 		System.out.println(newValue ? "Selected" : "Not Selected");
 		if (newValue) {
+			count_user();
+		} else {
 			counting.remove(EvalType.USER_DCI);
 			counting.remove(EvalType.USER_DII);
 			counting.remove(EvalType.USER_ADCI);
 			counting.remove(EvalType.USER_ADII);
-		} else {
-			count_user();
 		}
 	}
-	
+
 	private void count_user() {
-		
+		ArrayList<String> rules = new ArrayList<>();
+		ArrayList<String> op = new ArrayList<>();
+		for (int i = 0; i < userArray.size(); i++) {
+			if (i % 2 == 0)
+				rules.add(userArray.get(i));
+			else
+				op.add(userArray.get(i)); 
+		}
+		for (ExcelRecord r : recordList) {
+			ArrayList<Boolean> results = new ArrayList<>();
+			for (String rule : rules) {
+				results.add(applyRule(rule.split(" "), r));
+			}
+			counting.put(r.userEval(calcPass(results, op)),
+					counting.getOrDefault(r.userEval(calcPass(results, op)), 0) + 1);
+		}
+		System.out.println(rules);
+	}
+
+	private boolean calcPass(ArrayList<Boolean> bools, ArrayList<String> op) {
+		boolean end = false;
+		int index1, index2, index3;
+		while (bools.size() != 1) {
+			if (!end) {
+				index1 = 0;
+				index2 = 1;
+				index3 = 0;
+			} else {
+				index1 = bools.size()-1;
+				index2 = bools.size()-2;
+				index3 = op.size()-1;
+			}
+			switch (op.get(index3)) {
+			case "AND":
+				bools.set(index2, bools.get(index1) && bools.get(index2));
+				break;
+			case "OR":
+				bools.set(index2, bools.get(index1) || bools.get(index2));
+				break;
+			}
+			bools.remove(index1);
+			op.remove(index3);
+			end = !end;
+		}
+		return bools.get(0);
+	}
+
+	private boolean applyRule(String[] parts, ExcelRecord r) {
+		double val = 0;
+		switch(parts[0]) {
+		case "LOC":
+			val = (double) r.getLoc();
+			break;
+		case "CYCLO":
+			val = (double) r.getCyclo();
+			break;
+		case "ATFD":
+			val = (double) r.getAtfd();
+			break;
+		case "LAA":
+			val = (double) r.getLaa();
+			break;
+		}
+		double thresh = Double.parseDouble(parts[2]);
+		boolean pass = false;
+		switch(parts[1]) {
+		case "<":
+			pass = val < thresh;
+			break;
+		case ">":
+			pass = val > thresh;
+			break;
+		}
+		return pass;
 	}
 
 	public void importAction() {
@@ -201,11 +276,11 @@ public class PrimaryController implements Initializable {
 	}
 
 	public void graficoAction() {
-		
+
 		if(recordList == null) {
 			return;
 		}
-		
+
 		ChartController graphcontrol = new ChartController(counting);
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(getClass().getResource("graphicsView.fxml"));
@@ -221,7 +296,7 @@ public class PrimaryController implements Initializable {
 			e.printStackTrace();
 		}
 	}
-	
+
 
 	private void getRecordList() {
 		for (int row = 1; row < sheet.getLastRowNum(); row++) {
@@ -252,6 +327,27 @@ public class PrimaryController implements Initializable {
 					pmd, is_feature_envy));
 		}
 	}
+
+	@FXML public void userRulesAction() {
+		UserRulesController userRules = new UserRulesController();
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("userRules.fxml"));
+		try {
+			loader.setController(userRules);
+			Scene scene = new Scene(loader.load(), 800, 400);
+			userWindow = new Stage();
+			userWindow.setMaximized(false);
+			userWindow.setTitle("Regras de Utilizador");
+			userWindow.setScene(scene);
+			userWindow.showAndWait();
+			userArray = userRules.getArray();
+			if (use_rules.isSelected())
+				count_user();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	/*
 	 * private void switchToSecondary() throws IOException {
 	 * App.setRoot("secondary"); }
